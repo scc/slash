537506780c2eb2048d43eabc692eb4f967e0cc1b
Brian Aker <brian@tangent.org>
2000-09-18 18:29:19 +0000

    Importing Log module

---
+++ Slash/Apache/Log/Log.pm
@@ -0,0 +1,45 @@
+package Slash::Apache::Log;
+
+use strict;
+use Slash::DB;
+use Slash::Utility;
+use Apache::Constants qw(:common);
+
+$Slash::Apache::Log::VERSION = '0.01';
+
+sub handler {
+	my ($r) = @_;
+	my $slashdb = getCurrentDB();
+	my $op = $r->notes('SLASH_LOG_OPERATION');
+	my $dat = $r->notes('SLASH_LOG_DATA');
+	$slashdb->createAccessLog($op, $dat);
+
+	return OK;
+}
+
+1;
+__END__
+
+=head1 NAME
+
+Slash::Apache::Log - Handles logging for slashdot
+
+=head1 SYNOPSIS
+
+  use Slash::Apache::Log;
+
+=head1 DESCRIPTION
+
+No method are provided. Basically this handles grabbing the
+data out of the Apache process and logging it to the
+database. 
+
+=head1 AUTHOR
+
+Brian Aker, brian@tangent.org
+
+=head1 SEE ALSO
+
+perl(1).
+
+=cut
