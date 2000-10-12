b25077d5c4b44a79a453b1f6ab8c3d3b047ec622
Brian Aker <brian@tangent.org>
2000-10-12 20:43:36 +0000

    404.pl templated :)

--- themes/slashcode/htdocs/404.pl
+++ themes/slashcode/htdocs/404.pl
@@ -24,9 +24,9 @@
 #  $Id$
 ###############################################################################
 use strict;
-use lib '../';
 use Slash;
 use Slash::Utility;
+use Slash::Display;
 
 sub main {
 	my $constants = getCurrentStatic();
@@ -38,29 +38,21 @@ sub main {
 
 	header("404 File Not Found", '', '404 File Not Found');
 
-	print "<H1>404 File Not Found</H1>\nThe requested URL ($url) is not found.\n";
-
 	my($new_url, $errnum) = fixHref($url, 1);
 
 	if ($errnum && $errnum !~ /^\d+$/) {
-	    print qq|<P>$errnum, so you probably want to be here: <A HREF="$new_url">$new_url</A>\n|;
-	} elsif ($errnum == 1) {
-		print "<P>Someone <I>probably</I> just forgot the \"http://\" part of the URL, and you might really want to be here: <A HREF=\"$new_url\">$new_url</A>.\n";
-	} elsif ($errnum == 2) {
-		print "<P>Someone <I>probably</I> just forgot the \"ftp://\" part of the URL, and you might really want to be here: <A HREF=\"$new_url\">$new_url</A>.\n";
-	} elsif ($errnum == 3) {
-		print "<P>Someone <I>probably</I> just forgot the \"mailto:\" part of the URL, and you might really want to be here: <A HREF=\"$new_url\">$new_url</A>.\n";
-	} elsif ($errnum == 6) {
-		print "<P>All of the older articles have been moved to /articles/older, so you probably want to be here: <A HREF=\"$new_url\">$new_url</A>.\n";
-	} elsif ($errnum == 7) {
-		print "<P>All of the older features have been moved to /features/older, so you probably want to be here: <A HREF=\"$new_url\">$new_url</A>.\n";
-	} elsif ($errnum == 8) {
-		print "<P>All of the older book reviews have been moved to /books/older, so you probably want to be here: <A HREF=\"$new_url\">$new_url</A>.\n";
-	} elsif ($errnum == 9) {
-		print "<P>All of the older Ask $constants->{sitename} articles have been moved to /askslashdot/older, so you probably want to be here: <A HREF=\"$new_url\">$new_url</A>.\n";
+		slashDisplay('404-main', {
+			url => $new_url,
+			origin => $url,
+			message => $errnum,
+		});
+	} else {
+		slashDisplay('404-main', {
+			error  => $errnum,
+			url => $new_url,
+			origin => $url,
+		});
 	}
-
-	print "<P>If you feel like it, mail the url, and where ya came from to <A HREF=\"mailto:$constants->{adminmail}\">$admin</A>\n";
 
 	writeLog("404","404");
 	footer();
