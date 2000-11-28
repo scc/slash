c230ad50a04e8ad5d0953af9a215cd8f10d94986
Brian Aker <brian@tangent.org>
2000-11-28 17:24:20 +0000

    Reversed order.

--- Slash/DB/DB.pm
+++ Slash/DB/DB.pm
@@ -41,9 +41,8 @@ sub new {
 			require Slash::DB::PostgreSQL;
 			require Slash::DB::MySQL;
 
-# is this the right order?  that puts MySQL _before_ PostgreSQL -- pudge
-			push(@Slash::DB::ISA, 'Slash::DB::MySQL');
 			push(@Slash::DB::ISA, 'Slash::DB::PostgreSQL');
+			push(@Slash::DB::ISA, 'Slash::DB::MySQL');
 #			unless ($ENV{GATEWAY_INTERFACE}) {
 #				require Slash::DB::Static::PostgreSQL;
 #				push(@Slash::DB::ISA, 'Slash::DB::Static::PostgreSQL');
