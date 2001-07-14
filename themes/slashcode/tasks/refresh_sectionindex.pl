8a49a0012e86536387cdc043bd49f70d802acd68
Cliff Wood <clifton.wood@gmail.com>
2001-07-14 18:18:42 +0000

    Used to generate a section index for a given site. Can be overrided on a site-by-site basis with a bit of clever coding, but this is what's used for Slashdot (and a similar one can be constructed for Slashcode).
    Needs the "sectionindex;misc;default" template

---
+++ themes/slashcode/tasks/refresh_sectionindex.pl
@@ -0,0 +1,30 @@
+# refresh_sectionindex.pl
+# SlashD Task (c) OSDN 2001
+# $Id$
+#!/usr/bin/perl -w
+
+use strict;
+my $me = 'refresh_sectionindex.pl';
+
+use vars qw( %task );
+
+$task{$me}{timespec} = '*/30 * * * *';
+$task{$me}{code} = sub {
+	my $sections = getSectionInfo();
+
+	my $new_template = slashDisplay('sectionindex', {
+		sections => $sections,
+	}, 1);
+
+	my $tpid = $slashdb->getTemplateByName('sectionindex_display','tpid');
+	my %template = ( tpid => $tpid, template => $new_template );
+	# If it exists, we update it, if not, we create it.
+	if ($tpid) {
+		$slashdb->setTemplate($tpid, \%template);
+	} else {
+		$slashdb->createTemplate(\%template);
+	}
+}
+
+1;
+
