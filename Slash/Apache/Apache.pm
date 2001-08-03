78d93bda3b14414122f07ef64533156ef64fde93
Brian Aker <brian@tangent.org>
2001-08-03 20:13:52 +0000

    Simplified the regular expression in the IndexHandler (the other one was not working)

--- Slash/Apache/Apache.pm
+++ Slash/Apache/Apache.pm
@@ -108,7 +108,8 @@ sub IndexHandler {
 		my $basedir   = $constants->{basedir};
 
 		# cookie data will begin with word char or %
-		if ($r->header_in('Cookie') =~ /\b(?:user)=[%\w]/) {
+		#if ($r->header_in('Cookie') =~ /\b(?:user)=[%\w]/) {
+		if ($r->header_in('Cookie') =~ /(?:user/) {
 			$r->uri('/index.pl');
 			$r->filename("$basedir/index.pl");
 			return OK;
