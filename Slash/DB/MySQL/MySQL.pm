3c10ee52e892fff68f5fe12cc33072ad7addcc7e
Jamie McCarthy <jamie@mccarthy.vg>
2001-09-17 21:21:57 +0000

    Restore countUsers (now counts on users_count, not users).

--- Slash/DB/MySQL/MySQL.pm
+++ Slash/DB/MySQL/MySQL.pm
@@ -2793,6 +2793,13 @@ sub getModeratorLogRandom {
 }
 
 ########################################################
+sub countUsers {
+      my($self) = @_;
+      my($users) = $self->sqlSelect("count(*)", "users_count");
+      return $users;
+}
+
+########################################################
 sub countStoriesTopHits {
 	my($self) = @_;
 	my $story_table = getCurrentStatic('mysql_heap_table') ? 'story_heap' : 'stories';
