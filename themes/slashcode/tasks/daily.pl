1c1514241ba87403bd38d07bcd48a29b2eba535f
Cliff Wood <clifton.wood@gmail.com>
2001-10-16 16:56:42 +0000

    Change of time for the mailing list default.

--- themes/slashcode/tasks/daily.pl
+++ themes/slashcode/tasks/daily.pl
@@ -5,10 +5,10 @@ use strict;
 use vars qw( %task $me );
 
 # Remember that timespec goes by the database's time, which should be
-# GMT if you installed everything correctly.  So 6:07 AM GMT is a good
+# GMT if you installed everything correctly.  So 2:00 AM GMT is a good
 # sort of midnightish time for the Western Hemisphere.  Adjust for
 # your audience and admins.
-$task{$me}{timespec} = '7 6 * * *';
+$task{$me}{timespec} = '0 2 * * *';
 $task{$me}{timespec_panic_2} = ''; # if major panic, dailyStuff can wait
 $task{$me}{code} = sub {
 
