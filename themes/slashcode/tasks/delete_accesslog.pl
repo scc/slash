74f0756d305ad8424a0e7948774df5055b86eba6
Jamie McCarthy <jamie@mccarthy.vg>
2008-06-03 11:20:14 -0400

    Make delete_accesslog support a BLACKHOLE master->normal slave
    The DELETEs sent to the log master don't replicate to the slave if the
    master is ENGINE=BLACKHOLE and the slave is (obviously) not.  This checks
    for that situation by testing whether an id exists on the master, and
    if not, sends the DELETEs directly to the slave.

--- themes/slashcode/tasks/delete_accesslog.pl
+++ themes/slashcode/tasks/delete_accesslog.pl
@@ -20,7 +20,7 @@ use vars qw( %task $me );
 $task{$me}{timespec} = '22 * * * *'; # Normally run once an hour
 $task{$me}{timespec_panic_1} = '20 1,2,3,4,5,6 * * *'; # Just run at night if an issue pops up
 $task{$me}{timespec_panic_2} = ''; # In a pinch don't do anything
-$task{$me}{resource_locks} = { logdb => 1 };
+$task{$me}{resource_locks} = { logdb => 1, log_slave => 1 };
 $task{$me}{fork} = SLASHD_NOWAIT;
 $task{$me}{code} = sub {
 	my($virtual_user, $constants, $slashdb, $user) = @_;
@@ -37,6 +37,13 @@ $task{$me}{code} = sub {
 		return "nothing to do";
 	}
 
+	# If the log master is ENGINE=BLACKHOLE, we can't delete from there;
+	# delete from the log slave instead.
+	my $delete_db = $logdb;
+	if (! $logdb->sqlSelect('id', 'accesslog' "id=$id") {
+		$delete_db = $log_slave;
+	}
+
 	my $rows;
 	my $total = 0;
 	my $limit = 100_000;
@@ -44,14 +51,14 @@ $task{$me}{code} = sub {
 	my $last_err = "";
 	my $done = 0;
 	MAINLOOP: while (!$done) {
-		while ($rows = $logdb->sqlDelete("accesslog", "id < $id", $limit)) {
+		while ($rows = $delete_db->sqlDelete("accesslog", "id < $id", $limit)) {
 			$total += $rows;
 			last if $rows eq "0E0";
 			slashdLog("deleted so far $total of $limit rows");
 			sleep 10;
 		}
 		my $err = "";
-		if ( $counter >= $failures || !($err = $logdb->sqlError()) ) {
+		if ( $counter >= $failures || !($err = $delete_db->sqlError()) ) {
 			# If either we're giving up because there are too many
 			# failures, or the last attempt was successful, then
 			# break out of the loop, we're done.
