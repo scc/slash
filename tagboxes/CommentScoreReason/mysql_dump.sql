c52391cc7608097010d8aa697c8fc419f14360fc
Jamie McCarthy <jamie@mccarthy.vg>
2008-07-08 22:26:10 -0400

    Insert some needy comments into the hose
    Based on vars, a certain fraction of comments with a minimum level
    of neediness will be inserted into the hose once their neediness
    crosses that threshold (so they can be metamoderated).  Also, all
    comments inserted into the hose get their popularity and editorpop
    scores immediately recalculated.

--- tagboxes/CommentScoreReason/mysql_dump.sql
+++ tagboxes/CommentScoreReason/mysql_dump.sql
@@ -3,4 +3,6 @@ INSERT INTO tagboxes (tbid, name, affected_type, clid, weight, last_run_complete
 #INSERT INTO tagbox_userkeyregexes VALUES ('CommentScoreReason', '^tag_clout$');
 
 INSERT INTO vars (name, value, description) VALUES ('tagbox_csr_baseneediness', '60', 'Base neediness score for comments that have been moderated');
+INSERT INTO vars (name, value, description) VALUES ('tagbox_csr_minneediness', '138', 'Minimum neediness score to possibly insert a needy comment into the firehose');
+INSERT INTO vars (name, value, description) VALUES ('tagbox_csr_needinesspercent', '5', 'Percentage of comments with the necessary minimum neediness which actually do get inserted into the firehose');
 
